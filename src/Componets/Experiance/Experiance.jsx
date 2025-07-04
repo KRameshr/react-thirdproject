88266018e93d830059effa3d241af0478c7d2e0d
Hi, I'm K Ramesh <115610243+KRameshr@users.noreply.github.com>
2025-07-04 07:09:08 +0530

    Add files via upload

---
+++ src/Componets/Experiance/Experiance.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Experiance.css";
+const Experiance = () => {
+  return (
+    <div className="experiance" id="Experience">
+      <div className="achievement">
+        <div className="circle">1+</div>
+        <span>years</span>
+        <span>Experiance</span>
+      </div>
+      <div className="achievement">
+        <div className="circle">5+</div>
+        <span>Completed</span>
+        <span>Projects</span>
+      </div>
+      {/* <div className="achievement">
+        <div className="circle">5</div>
+        <span>Companies</span>
+        <span>Works</span>
+      </div> */}
+    </div>
+  );
+};
+
+export default Experiance;
